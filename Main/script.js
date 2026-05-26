70cd872d8b5c037d819cd93b01761999e21f785b
Daniel Gama <danielgama.dev@gmail.com>
2026-05-26 09:01:08 -0300

    Primeiro commit

---
+++ Main/script.js
@@ -0,0 +1,86 @@
+// SENHAS
+
+const senhaUsuario = "1013947"
+const senhaTI = "258852"
+
+// LOGIN
+
+function entrarSistema(){
+
+  const senha = document.getElementById("senha").value
+
+  const erro = document.getElementById("erro")
+
+  if(
+    senha === senhaUsuario ||
+    senha === senhaTI
+  ){
+
+    document.getElementById("loginArea")
+    .style.display = "none"
+
+    document.getElementById("sistema")
+    .style.display = "block"
+
+  }else{
+
+    erro.innerText = "Senha inválida"
+
+  }
+
+}
+
+// FORMULÁRIO
+
+document
+.getElementById("ticketForm")
+.addEventListener("submit", async function(e){
+
+  e.preventDefault()
+
+  const btn =
+  document.getElementById("btnEnviar")
+
+  btn.disabled = true
+  btn.innerText = "Enviando..."
+
+  const dados = {
+
+    titulo: this.titulo.value,
+    descricao: this.descricao.value,
+    nome: this.nome.value,
+    setor: this.setor.value,
+    urgencia: this.urgencia.value,
+    importancia: this.importancia.value
+
+  }
+
+  try{
+
+    // URL DA API APPS SCRIPT
+    const response = await fetch("COLE_SUA_URL_AQUI", {
+
+      method:"POST",
+
+      body: JSON.stringify(dados)
+
+    })
+
+    const resultado = await response.json()
+
+    alert(resultado.message)
+
+    this.reset()
+
+  }catch(error){
+
+    alert("Erro ao enviar ticket.")
+
+    console.error(error)
+
+  }
+
+  btn.disabled = false
+  btn.innerText = "Abrir Ticket"
+
+})
